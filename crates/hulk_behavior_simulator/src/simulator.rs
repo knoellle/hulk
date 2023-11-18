ea3c3e2847102809607fb79eeb80d561a716fdaf
knoellle <konrad@familie-noelle.de>
2023-11-18 21:29:24 +0100

    Clean up game state transitions

--- crates/hulk_behavior_simulator/src/simulator.rs
+++ crates/hulk_behavior_simulator/src/simulator.rs
@@ -15,42 +15,22 @@ pub type Callback = Arc<dyn FnMut(&mut State) + Send + Sync>;
 
 #[derive(Default)]
 pub struct Simulator {
-    pub state: State,
     pub frames: Vec<Frame>,
     pub callback: Option<Callback>,
 }
 
 impl Simulator {
-    pub fn run(&mut self) -> Result<()> {
-        loop {
-            self.cycle()?;
-            if self.state.finished {
-                break;
-            }
-        }
-
-        Ok(())
-    }
-
-    pub fn register_callback(&mut self, f: Callback) {
-        self.callback.replace(f);
-    }
-
-    pub fn cycle(&mut self) -> Result<()> {
-        self.state.cycle(Duration::from_millis(12))?;
+    pub fn cycle(&mut self, state: &mut State) -> Result<()> {
+        state.cycle(Duration::from_millis(12))?;
 
         let mut robots = Players::<Option<Database>>::default();
-        for (player_number, robot) in &self.state.robots {
+        for (player_number, robot) in &state.robots {
             robots[*player_number] = Some(robot.database.clone())
         }
         self.frames.push(Frame {
             robots,
-            ball: self.state.ball.clone(),
+            ball: state.ball.clone(),
         });
-
-        if self.state.cycle_count > 10_000 {
-            self.state.finished = true;
-        }
 
         Ok(())
     }
--- tools/behavior_simulator/src/bin/golden_goal.rs
+++ tools/behavior_simulator/src/bin/golden_goal.rs
@@ -1,11 +1,16 @@
-use behavior_simulator::{server, simulator::Simulator, state::Ball};
-use spl_network_messages::{GameState, PlayerNumber, Team};
+use behavior_simulator::{
+    server,
+    simulator::Simulator,
+    state::{Ball, State},
+};
+use spl_network_messages::{PlayerNumber, Team};
 use tokio::time::Instant;
 use tokio_util::sync::CancellationToken;
 use types::filtered_game_state::FilteredGameState;
 
 fn main() -> color_eyre::Result<()> {
     let mut simulator = Simulator::default();
+    let mut state = State::default();
 
     for player_number in [
         PlayerNumber::One,
@@ -16,38 +21,46 @@ fn main() -> color_eyre::Result<()> {
         PlayerNumber::Six,
         PlayerNumber::Seven,
     ] {
-        simulator.state.spawn_robot(player_number)?.is_penalized = false;
+        state.spawn_robot(player_number)?;
     }
 
-    simulator.state.game_controller_state.game_state = GameState::Ready;
-    simulator.state.filtered_game_state = FilteredGameState::Ready {
-        kicking_team: Team::Hulks,
-    };
+    let mut goals = 0;
 
     let start = Instant::now();
     for cycle in 0..10000 {
-        if cycle % 1000 == 0 && simulator.state.ball.is_none() {
-            simulator.state.ball = Some(Ball::default());
+        if cycle == 100 {
+            for robot in state.robots.values_mut() {
+                robot.is_penalized = false;
+            }
+            state.filtered_game_state = FilteredGameState::Ready {
+                kicking_team: Team::Hulks,
+            }
         }
-
         if cycle == 1600 {
-            simulator.state.filtered_game_state = FilteredGameState::Set;
+            state.filtered_game_state = FilteredGameState::Set;
         }
-        if cycle % 2000 == 0 {
-            simulator.state.filtered_game_state = FilteredGameState::Playing { ball_is_free: true };
+        if cycle == 1700 {
+            state.ball = Some(Ball::default());
         }
 
-        if simulator
-            .state
+        if cycle % 2000 == 0 {
+            state.filtered_game_state = FilteredGameState::Playing { ball_is_free: true };
+        }
+
+        if state
             .ball
             .as_ref()
             .is_some_and(|ball| ball.position.x.abs() > 4.5 && ball.position.y < 0.75)
         {
-            simulator.state.ball = None;
-            simulator.state.filtered_game_state = FilteredGameState::Set;
+            goals += 1;
+            state.ball = None;
+            state.filtered_game_state = FilteredGameState::Set;
+        }
+        if goals > 0 {
+            break;
         }
 
-        simulator.cycle()?;
+        simulator.cycle(&mut state)?;
     }
     let duration = Instant::now() - start;
     println!("Took {:.2} seconds", duration.as_secs_f32());
