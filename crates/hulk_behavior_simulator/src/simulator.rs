d08f55f2f665480e79e196752edebc277d89e01b
Konrad Nölle <konrad@familie-noelle.de>
2024-05-21 16:35:39 +0200

    Working scenario

--- crates/hulk_behavior_simulator/src/simulator.rs
+++ crates/hulk_behavior_simulator/src/simulator.rs
@@ -1,7 +1,8 @@
-use std::{sync::Arc, time::Duration};
+use std::time::{Duration, SystemTime};
 
-use crate::{cyclers::control::Database, state::Ball};
+use crate::{cyclers::control::Database, server, state::Ball};
 use color_eyre::Result;
+use tokio_util::sync::CancellationToken;
 use types::players::Players;
 
 use crate::state::State;
@@ -11,18 +12,48 @@ pub struct Frame {
     pub robots: Players<Option<Database>>,
 }
 
-pub type Callback = Arc<dyn FnMut(&mut State) + Send + Sync>;
+pub trait Scenario {
+    fn init(&mut self, state: &mut State) -> Result<()>;
 
-#[derive(Default)]
-pub struct Simulator {
-    pub frames: Vec<Frame>,
-    pub callback: Option<Callback>,
+    fn cycle(&mut self, state: &mut State) -> Result<()> {
+        state.cycle(Duration::from_millis(12))
+    }
+
+    fn run(&mut self) -> Result<()> {
+        let mut state = State::default();
+        self.init(&mut state)?;
+        let mut recorder = Recorder::default();
+        let start = SystemTime::now();
+        while self.should_continue(&mut state) {
+            self.cycle(&mut state)?;
+            recorder.record(&mut state);
+        }
+        let duration = start.elapsed().expect("time ran backwards");
+        println!("Took {:.2}s", duration.as_secs_f32());
+        let cycles = state.cycle_count;
+        println!(
+            "{} cycles, {:.2} cycles/s",
+            cycles,
+            cycles as f32 / duration.as_secs_f32()
+        );
+
+        server::run(recorder.frames, Some("[::]:1337"), CancellationToken::new())?;
+
+        Ok(())
+    }
+
+    fn should_continue(&mut self, state: &mut State) -> bool {
+        !state.finished && state.cycle_count < 10_000
+    }
 }
 
-impl Simulator {
-    pub fn cycle(&mut self, state: &mut State) -> Result<()> {
-        state.cycle(Duration::from_millis(12))?;
+#[derive(Default)]
+pub struct Recorder {
+    pub frames: Vec<Frame>,
+}
 
+impl Recorder {
+    pub fn record(&mut self, state: &mut State) {
         let mut robots = Players::<Option<Database>>::default();
         for (player_number, robot) in &state.robots {
             robots[*player_number] = Some(robot.database.clone())
@@ -31,7 +62,5 @@ impl Simulator {
             robots,
             ball: state.ball.clone(),
         });
-
-        Ok(())
     }
 }
