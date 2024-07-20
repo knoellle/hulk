aaf01c6c7d1045028760511cced3ae4afe2cc6c9
Konrad Nölle <konrad@familie-noelle.de>
2024-07-20 22:26:26 +0200

    --wip--

--- crates/hulk_replayer/src/worker_thread.rs
+++ crates/hulk_replayer/src/worker_thread.rs
@@ -43,6 +43,21 @@ async fn replay_worker(
     update_callback: impl Fn() + Send + Sync + 'static,
 ) {
     let mut parameters_receiver = replayer.get_parameters_receiver();
+
+    let mut merged_frame_times = replayer
+        .get_recording_indices()
+        .values()
+        .enumerate()
+        .flat_map(|(instance, index)| index.iter().map(move |timing| (instance, timing.timestamp)))
+        .collect::<Vec<_>>();
+    merged_frame_times.sort_by_key(|(_, time)| *time);
+    let instance_names = replayer
+        .get_recording_indices()
+        .keys()
+        .cloned()
+        .collect::<Vec<_>>();
+    let mut last_time = receiver.borrow().time;
+
     loop {
         select! {
             _ = parameters_receiver.wait_for_change() => {}
@@ -55,9 +70,34 @@ async fn replay_worker(
             }
         }
 
-        let timestamp = receiver.borrow().time;
-        if let Err(error) = replayer.replay_at(timestamp) {
-            eprintln!("{error:#?}");
+        let new_time = receiver.borrow().time;
+        let end_index = match merged_frame_times.binary_search_by_key(&new_time, |item| item.1) {
+            Ok(index) => index,
+            Err(index) => index,
+        }
+        .min(merged_frame_times.len() - 1);
+
+        let start_index =
+            match merged_frame_times.binary_search_by_key(&last_time, |item| item.1) {
+                Ok(index) => index,
+                Err(index) => index,
+            }
+            // todo: replay other cyclers when going backwards
+            .min(end_index);
+        last_time = new_time;
+
+        for (instance_index, time) in &merged_frame_times[start_index..=end_index] {
+            let instance_name = &instance_names[*instance_index];
+            if let Ok(Some(frame)) = replayer
+                .get_recording_indices_mut()
+                .get_mut(instance_name)
+                .unwrap()
+                .find_latest_frame_up_to(*time)
+            {
+                if let Err(error) = replayer.replay(instance_name, *time, &frame.data) {
+                    eprintln!("{error:#?}");
+                }
+            }
         }
 
         update_callback()
